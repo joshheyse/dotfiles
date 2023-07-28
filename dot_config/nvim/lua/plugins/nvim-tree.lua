544408165b59641255462543ac2745e50f1d2add
Josh Heyse <josh@heyse.us>
2023-07-28 15:24:47 -0400

    Update .aliases Update .config/nvim/lazy-lock.json Update .config/nvim/lua/plugins/nvim-tree.lua Update .config/nvim/lua/plugins/treesitter.lua Update .gitconfig Update .tmux.conf

--- dot_config/nvim/lua/plugins/nvim-tree.lua
+++ dot_config/nvim/lua/plugins/nvim-tree.lua
@@ -1,3 +1,21 @@
+local function on_attach(bufnr)
+  local api = require "nvim-tree.api"
+
+  local function opts(desc)
+    return { desc = "nvim-tree: " .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
+  end
+
+  -- default mappings
+  api.config.mappings.default_on_attach(bufnr)
+
+  -- custom mappings
+  vim.keymap.set("n", "<C-t>", api.tree.change_root_to_parent, opts "Up")
+  vim.keymap.set("n", "?", api.tree.toggle_help, opts "Help")
+  -- nvim-tree-api.node.open.vertical()
+  vim.keymap.set("n", "v", api.node.open.vertical, opts "Open: Vertical Split")
+  vim.keymap.set("n", "s", api.node.open.horizontal, opts "Open: Vertical Split")
+end
+
 return {
   "nvim-tree/nvim-tree.lua",
   cmd = { "NvimTreeToggle" },
@@ -5,9 +23,10 @@ return {
     { "<leader>e", "<cmd>NvimTreeToggle<cr>", desc = "Explorer" },
   },
   opts = {
+    on_attach = on_attach,
     disable_netrw = false,
     hijack_netrw = true,
-    respect_buf_cwd = true,
+    respect_buf_cwd = false,
     view = {
       number = true,
       relativenumber = true,
@@ -15,7 +34,7 @@ return {
     filters = {
       custom = { ".git" },
     },
-    sync_root_with_cwd = true,
+    sync_root_with_cwd = false,
     update_focused_file = {
       enable = true,
       update_root = true,

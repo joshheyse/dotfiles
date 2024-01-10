81ec8b968cf641ca700dd3b286c977d30dcf56b4
Josh Heyse <josh@heyse.us>
2024-01-10 16:12:15 -0600

    Update .config/astronvim/lua/user/plugins/tokyonight.lua

--- dot_config/astronvim/lua/user/plugins/tokyonight.lua
+++ dot_config/astronvim/lua/user/plugins/tokyonight.lua
@@ -11,16 +11,16 @@ return {
       -- Style to be applied to different syntax groups
       -- Value is any valid attr-list value `:help attr-list`
       comments = "italic",
-      keywords = "NONE",
+      keywords = "bold",
       functions = "NONE",
       variables = "NONE",
       -- Background styles. Can be "dark", "transparent" or "normal"
-      sidebars = "transparent", -- style for sidebars, see below
+      sidebars = "transparent",       -- style for sidebars, see below
     },
-    day_brightness = 0.3, -- Adjusts the brightness of the colors of the **Day** style. Number between 0 and 1, from dull to vibrant colors                                                  |
+    day_brightness = 0.3,             -- Adjusts the brightness of the colors of the **Day** style. Number between 0 and 1, from dull to vibrant colors                                                  |
     hide_inactive_statusline = false, -- Enabling this option, will hide inactive statuslines and replace them with a thin border instead. Should work with the standard **StatusLine** and **LuaLine**. |
-    dim_inactive = true, -- dims inactive windows
-    lualine_bold = false, -- When `true`, section headers in the lualine theme will be bold|
+    dim_inactive = true,              -- dims inactive windows
+    lualine_bold = false,             -- When `true`, section headers in the lualine theme will be bold|
     on_highlights = function(hl, _)
       hl.TSRainbowRed = { link = "rainbowcol1" }
       hl.TSRainbowOrange = { link = "rainbowcol2" }
@@ -32,7 +32,7 @@ return {
     end,
   },
   config = function(_, opts)
-    local tokyonight = require "tokyonight"
+    local tokyonight = require("tokyonight")
     tokyonight.setup(opts)
     tokyonight.load()
   end,

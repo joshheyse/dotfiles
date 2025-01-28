df64dc11ba387b10d7f1624138c2268aa777d017
Josh Heyse <josh@heyse.us>
2025-01-28 10:07:36 -0600

    Update .config/nvim/lua/plugins/astrolsp.lua

--- dot_config/nvim/lua/plugins/astrolsp.lua
+++ dot_config/nvim/lua/plugins/astrolsp.lua
@@ -43,7 +43,18 @@ return {
     -- customize language server configuration options passed to `lspconfig`
     ---@diagnostic disable: missing-fields
     config = {
-      -- clangd = { capabilities = { offsetEncoding = "utf-8" } },
+      clangd = {
+        capabilities = { offsetEncoding = "utf-8" },
+        cmd = {
+          "clangd",
+          "--background-index",
+          "--clang-tidy",
+          "--header-insertion=iwyu",
+          "--completion-style=detailed",
+          "--function-arg-placeholders",
+          "--fallback-style=llvm",
+        },
+      },
     },
     -- customize how language servers are attached
     handlers = {

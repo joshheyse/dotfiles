87703d02137bb196ebb1b372e6765b48ab1bb710
Josh Heyse <josh@heyse.us>
2025-01-28 10:06:44 -0600

    Update .config/nvim/lua/plugins/nvim-osc52.lua

--- dot_config/nvim/lua/plugins/nvim-osc52.lua
+++ dot_config/nvim/lua/plugins/nvim-osc52.lua
@@ -2,5 +2,5 @@ return {
   "ojroques/nvim-osc52",
   name = "nvim-osc52",
   lazy = false,
-  enabled = vim.version().major == 0 and vim.version().minor < 10,
+  -- enabled = vim.version().major == 0 and vim.version().minor < 10,
 }
